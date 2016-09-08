0b761070bb526c7d9d0869a65e2bf6501ac71444
dmhacker <dmhacker@yahoo.com>
2016-09-08 16:41:23 -0700

    Test

--- app.js
+++ app.js
@@ -15,13 +15,10 @@ app.get('/', function(request, response) {
 
 app.use('/proxy', proxy('www.google.com'));
 
-/*
 app.get('/site/:b64url', function(req, res) {
     var website = new Buffer(req.params.b64url, 'base64').toString('ascii');
-
-
+    return proxy(website)(req, res);
 });
-*/
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
