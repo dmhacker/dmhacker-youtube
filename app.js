c480b3752c1b70b58107e3ac6e57d54f25912568
dmhacker <dmhacker@yahoo.com>
2016-09-08 11:45:52 -0700

    Simple proxy

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var proxy = require('express-http-proxy');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,9 +10,19 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('index');
 });
 
+app.get('/proxy', proxy('www.google.com'));
+
+/*
+app.get('/site/:b64url', function(req, res) {
+    var website = new Buffer(req.params.b64url, 'base64').toString('ascii');
+
+
+});
+*/
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
