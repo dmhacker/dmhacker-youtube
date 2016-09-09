699f941b2de62de6cc220ce21aba24d99a859ac1
dmhacker <dmhacker@yahoo.com>
2016-09-08 20:27:17 -0700

    Exists method

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,8 +1,21 @@
 var app = angular.module('Proxy', []);
 
-app.controller('ProxyController', function($scope) {
+app.controller('ProxyController', function($scope, $http) {
 
     $scope.go = function () {
-        window.location.href = '/site/' + window.btoa($scope.url);
+        var b64url = window.btoa($scope.url);
+        $http({
+            method: 'GET',
+            url: '/exists/'+b64url
+        }).then(function (resp) {
+            if (resp.data.exists) {
+                window.location.href = '/site/' + b64url;
+            }
+            else {
+                Materialize.toast('That site does not exist!', 4000);
+            }
+        }, function (resp) {
+            Materialize.toast(resp.data.message || 'An error occurred during processing.', 4000);
+        });
     };
 });
