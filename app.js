ddd3a740ed238f15a9a4017421e7fc9cd385698d
dmhacker <dmhacker@yahoo.com>
2016-09-08 19:09:08 -0700

    Confuse

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get('/', function(request, response) {
 app.get('/site/:b64url', function (req, res) {
     var urlObject = require('url').parse(new Buffer(req.params.b64url, 'base64').toString('ascii'));
     var urlHost = urlObject.protocol + (urlObject.slashes ? '//' : '') + urlObject.host;
+    console.log(new Buffer(req.params.b64url, 'base64').toString('ascii'));
     console.log(urlObject);
     console.log(urlHost);
     console.log(urlObject.path);
