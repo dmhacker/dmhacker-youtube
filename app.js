3812108f328bf953e81ae58295aa5eb0cb24eaf8
dmhacker <dmhacker@yahoo.com>
2016-09-09 06:55:51 -0700

    Try just using regular proxy

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({
+    target: 'http://localhost:'+app.get('port')
+}).listen(8000);
+
+//
+// Create your target server
+//
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+    res.end();
+}).listen(app.get('port'));
+
+/*
 var express = require('express');
 var proxy = require('express-http-proxy');
 var urlExists = require('url-exists');
@@ -51,3 +72,4 @@ app.get('/site/:b64url', function(req, res) {
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+*/
