557f7694252458c99cf2555d2e44a8f8590d5b46
dmhacker <dmhacker@yahoo.com>
2016-09-08 19:01:05 -0700

    Proxy request modification

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express');
-var http = require('express-http-proxy');
+var proxy = require('express-http-proxy');
 
 var app = express();
-var proxy = httpProxy.createProxyServer();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,21 +14,16 @@ app.get('/', function(request, response) {
     response.render('index');
 });
 
-app.get('/site/:b64url', proxy('www.google.com', {
-    forwardPath: function(req, res) {
-        return require('url').parse(new Buffer(req.params.b64url, 'base64').toString('ascii')).path;
-    }
-}));
-
-/*
-http.createServer(function(req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
-    res.end();
-}).listen(8080);
-*/
+// Where the magic happens
+app.get('/site/:b64url', function (req, res) {
+    var urlObject = require('url').parse(new Buffer(req.params.b64url, 'base64').toString('ascii'));
+    var urlHost = urlObject.protocol + (urlObject.slashes ? '//' : '') + urlObject.host;
+    proxy(urlHost, {
+        forwardPath: function(req, res) {
+            return urlObject.path;
+        }
+    })(req, res);
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
