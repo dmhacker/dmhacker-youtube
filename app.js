c7973a2b67b38ed6f8277ced0c15b80dce94f4b4
dmhacker <dmhacker@yahoo.com>
2016-11-28 19:58:38 -0800

    Track history

--- app.js
+++ app.js
@@ -26,12 +26,23 @@ app.get('/', function(request, response) {
 app.get('/target/:id', function (req, res) {
     var id = req.params.id;
     var old_url = 'http://www.youtube.com/watch?v='+id;
-    var new_url = path.join(__dirname, 'public', 'site', id+'.mp4');
-    var writeable = ytdl(old_url).pipe(fs.createWriteStream(new_url));
-    writeable.on('finish', function () {
-        res.status(200).json({
-            link: '/site/'+id+'.mp4'
-        });
+    ytdl.getInfo(old_url, function (err, info) {
+        if (err) {
+            res.status(500).send(err.message);
+        }
+        else {
+            var new_url = path.join(__dirname, 'public', 'site', id+'.mp4');
+            var writeable = ytdl(old_url).pipe(fs.createWriteStream(new_url));
+            writeable.on('finish', function () {
+                res.status(200).json({
+                    link: '/site/'+id+'.mp4',
+                    info: {
+                        id: id,
+                        title: info.title
+                    }
+                });
+            });
+        }
     });
 });
 
--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,7 +1,14 @@
-var app = angular.module('Proxy', []);
+var app = angular.module('Proxy', ["ngCookies"]);
 
-app.controller('ProxyController', function($scope, $http) {
+app.controller('ProxyController', function($scope, $http, $cookies) {
     $scope.loading = false;
+
+    if ($cookies.getObject('dmhackerYoutube')) {
+        var dmhackerYoutube = $cookies.getObject('dmhackerYoutube');
+        $scope.history = dmhackerYoutube.history;
+    } else {
+        $scope.history = [];
+    }
 
     $scope.view = function () {
         $scope.loading = true;
@@ -11,15 +18,33 @@ app.controller('ProxyController', function($scope, $http) {
             url: '/target/'+id
         }).then(function (resp) {
             if (resp.data.link) {
+                $scope.history.unshift(resp.data.info);
+                $scope.save();
                 window.location.href = resp.data.link;
             }
             else {
                 Materialize.toast('That video does not exist!', 4000);
+                $scope.loading = false;
             }
-            $scope.loading = false;
         }, function (resp) {
             Materialize.toast(resp.data.message || 'An error occurred during processing.', 4000);
             $scope.loading = false;
         });
+    };
+
+    $scope.save = function () {
+        var now = new Date(),
+            exp = new Date(now.getFullYear(), now.getMonth() + 6, now.getDate());
+
+        $cookies.putObject('dmhackerYoutube', {
+            history: $scope.history
+        }, {
+            expires: exp
+        });
+    };
+
+    $scope.clear = function () {
+        $cookies.remove('dmhackerYoutube');
+        $scope.history = [];
     };
 });
