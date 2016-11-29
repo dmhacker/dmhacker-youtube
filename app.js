f6e084723552fb91dcc4537afba9a21a56a9d283
dmhacker <dmhacker@yahoo.com>
2016-11-28 18:48:37 -0800

    File type

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.get('/', function(request, response) {
 app.get('/target/:id', function (req, res) {
     var id = req.params.id;
     var old_url = 'http://www.youtube.com/watch?v='+id;
-    var new_url = path.join(__dirname, 'public', 'site', id+'.wlv');
+    var new_url = path.join(__dirname, 'public', 'site', id+'.mp4');
     console.log(new_url);
     ytdl(old_url).pipe(fs.createWriteStream(new_url));
     res.status(200).json({
-        link: '/site/'+id
+        link: '/site/'+id+'.mp4'
     });
 });
 
