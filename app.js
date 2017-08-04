19c3900bb3a2cf89f9fd37c4c3f09d799bd4baa4
dmhacker <dmhacker@yahoo.com>
2017-08-04 09:19:58 -0700

    No longer use S3 - use tmp storage on Heroku

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
@@ -8,25 +6,25 @@ var s3 = require('s3');
 var search = require('youtube-search');
 
 var searchOpts = {
-    maxResults: 1,
-    type: 'video',
-    key: process.env.YOUTUBE_API_KEY
+  maxResults: 1,
+  type: 'video',
+  key: process.env.YOUTUBE_API_KEY
 };
 
 global.__bucket = process.env.S3_BUCKET;
 
 var s3Client = s3.createClient({
-    s3Options: {
-        accessKeyId: process.env.S3_ACCESS_KEY,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
-    }
+  s3Options: {
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+  }
 });
 
 var app = express();
 
 var dir = './public/site';
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
 }
 
 app.set('port', (process.env.PORT || 5000));
@@ -37,101 +35,115 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('index');
+  response.render('index');
 });
 
-app.get('/alexa/:id', function (req, res) {
-    var id = req.params.id;
-    var old_url = 'https://www.youtube.com/watch?v='+id;
-    ytdl.getInfo(old_url, function (err, info) {
-        if (err) {
-            res.status(500).json({
-                state: 'error',
-                message: err.message
-            });
-        }
-        else {
-            var tmpfile = require('path').join('/tmp', id+'.mp3');
-            var key = require('path').join('audio', id+'.mp3');
+app.get('/alexa/:id', function(req, res) {
+  var id = req.params.id;
+  var old_url = 'https://www.youtube.com/watch?v=' + id;
+  ytdl.getInfo(old_url, function(err, info) {
+    if (err) {
+      res.status(500).json({
+        state: 'error',
+        message: err.message
+      });
+    } else {
+      var new_url = path.join(__dirname, 'public', 'site', id + '.mp3');
+      var writer = fs.createWriteStream(new_url);
+      writer.on('finish', function() {
+        res.status(200).json({
+          state: 'success',
+          link: 'https://dmhacker-youtube.herokuapp.com/site/' + id + '.mp3',
+          info: {
+            id: id,
+            title: info.title
+          }
+        });
+      });
+      ytdl(old_url, {
+        filter: 'audioonly'
+      }).pipe(new_url);
+      /*
+      var tmpfile = require('path').join('/tmp', id+'.mp3');
+      var key = require('path').join('audio', id+'.mp3');
 
-            var writer = fs.createWriteStream(tmpfile);
-            writer.on('finish', function () {
-                var uploader = s3Client.uploadFile({
-                    localFile: tmpfile,
-                    s3Params: {
-                        Bucket: __bucket,
-                        Key: key
-                    }
-                });
-                uploader.on('end', function() {
-                    res.status(200).json({
-                        state: 'success',
-                        message: 'Uploaded successfully.',
-                        link: s3.getPublicUrl(__bucket, key, 'us-west-1')
-                    });
-                });
-            });
-            ytdl(old_url, {
-                filter: 'audioonly'
-            }).pipe(writer);
-        }
-    });
+      var writer = fs.createWriteStream(tmpfile);
+      writer.on('finish', function () {
+          var uploader = s3Client.uploadFile({
+              localFile: tmpfile,
+              s3Params: {
+                  Bucket: __bucket,
+                  Key: key
+              }
+          });
+          uploader.on('end', function() {
+              res.status(200).json({
+                  state: 'success',
+                  message: 'Uploaded successfully.',
+                  link: s3.getPublicUrl(__bucket, key, 'us-west-1')
+              });
+          });
+      });
+      ytdl(old_url, {
+          filter: 'audioonly'
+      }).pipe(writer);
+      */
+    }
+  });
 });
 
 function fetch_target_id(req, res) {
-    var id = req.params.id;
-    var old_url = 'https://www.youtube.com/watch?v='+id;
-    ytdl.getInfo(old_url, function (err, info) {
-        if (err) {
-            res.status(500).json({
-                state: 'error',
-                message: err.message
-            });
-        }
-        else {
-            var new_url = path.join(__dirname, 'public', 'site', id+'.mp4');
-            var writeable = ytdl(old_url).pipe(fs.createWriteStream(new_url));
-            writeable.on('finish', function () {
-                res.status(200).json({
-                    state: 'success',
-                    link: '/site/'+id+'.mp4',
-                    info: {
-                        id: id,
-                        title: info.title
-                    }
-                });
-            });
-        }
-    });
+  var id = req.params.id;
+  var old_url = 'https://www.youtube.com/watch?v=' + id;
+  ytdl.getInfo(old_url, function(err, info) {
+    if (err) {
+      res.status(500).json({
+        state: 'error',
+        message: err.message
+      });
+    } else {
+      var new_url = path.join(__dirname, 'public', 'site', id + '.mp4');
+      var writer = fs.createWriteStream(new_url);
+      writer.on('finish', function () {
+        res.status(200).json({
+          state: 'success',
+          link: '/site/' + id + '.mp4',
+          info: {
+            id: id,
+            title: info.title
+          }
+        });
+      });
+      ytdl(old_url).pipe(writer);
+    }
+  });
 }
 
 app.get('/target/:id', fetch_target_id);
 
-app.get('/search/:query', function (req, res) {
-    var query = req.params.query;
-    search(query, searchOpts, function(err, results) {
-        if (err) {
-            res.status(500).json({
-                state: 'error',
-                message: err.message
-            });
-        }
-        else {
-            if (!results || !results.length) {
-                res.status(200).send({
-                    state: 'error',
-                    message: 'No results found'
-                });
-            }
-            else {
-                var id = results[0].id;
-                req.params.id = id;
-                fetch_target_id(req, res);
-            }
-        }
-    });
+app.get('/search/:query', function(req, res) {
+  var query = req.params.query;
+  search(query, searchOpts, function(err, results) {
+    if (err) {
+      res.status(500).json({
+        state: 'error',
+        message: err.message
+      });
+    } else {
+      if (!results || !results.length) {
+        res.status(200).send({
+          state: 'error',
+          message: 'No results found'
+        });
+      } else {
+        var id = results[0].id;
+        req.params.id = id;
+        fetch_target_id(req, res);
+      }
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
