8c735b8765a03c499dd48cc2a02dd349a00da300
David Hacker <dmhacker@protonmail.com>
2018-12-02 10:46:19 -0800

    Better logging system; refractor APIs; prepare for alexa-youtube-skill v3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
-var ytdl = require('ytdl-core');
-var ytsearch = require('youtube-search');
+
+// Load logging utilities
+process.env.LOG_LEVEL = 'info';
+require("log-node")();
+var log = require('log').get('youtube-portal')
 
 // Create express server
 var app = express();
@@ -22,195 +25,15 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
-const YOUTUBE_URL_PREFIX = "https://www.youtube.com/watch?v=";
-
-//////////////////////////// ALEXA ROUTES ////////////////////////////
-
+// Setup a cache; used for holding video links
 var cache = {};
 
-app.get('/alexa-search/:query', function(req, res) {
-  // Extract query and language (English is default)
-  var query = new Buffer(req.params.query, 'base64').toString();
-  var lang = req.query.language || 'en';
-
-  console.log('Query from ' + req.connection.remoteAddress +
-    ': [' + lang + '] ' +  query);
-
-  // Perform search
-  ytsearch(query, {
-    maxResults: 1,
-    type: 'video',
-    relevanceLanguage: lang,
-    key: process.env.YOUTUBE_API_KEY
-  }, function(err, results) {
-    if (err) {
-      console.error('An error occurred: ' + err.message);
-
-      // Catastrophic error occurred
-      res.status(500).json({
-        state: 'error',
-        message: err.message
-      });
-    } else if (!results || !results.length) {
-      console.log('No results found.');
-
-      // No results found but no error; highly unlikely to occur
-      res.status(200).send({
-        state: 'error',
-        message: 'No results found'
-      });
-    } else {
-      // Extract metadata from the search results
-      var metadata = results[0];
-      var id = metadata.id;
-      var title = metadata.title;
-      var url = YOUTUBE_URL_PREFIX + id;
-
-      console.log('Query result: ' + title);
-
-      if (!(id in cache)) {
-        console.log("Starting download ... " + title);
-
-        // Mark video as 'not downloaded' in the cache
-        cache[id] = { downloaded: false };
-
-        // Create writer to output file for downloaded audio
-        var output_file = path.join(__dirname, 'public', 'site', id + '.m4a');
-        var writer = fs.createWriteStream(output_file);
-
-        // Pass writer stream to ytdl
-        ytdl(url, {
-          filter: 'audioonly',
-          quality: '140'
-        }).pipe(writer);
-
-        // Mark video as downloaded once writer is finished
-        writer.on('finish', function() {
-            console.log("Finished download ... " + title);
-
-            // Mark video as completed
-            cache[id]['downloaded'] = true;
-        });
-      }
-
-      // Return correctly and download the audio in the background
-      res.status(200).json({
-        state: 'success',
-        message: 'Uploaded successfully.',
-        link: '/site/' + id + '.m4a',
-        info: {
-          id: id,
-          title: metadata.title,
-          original: url
-        }
-      });
-    }
-  });
-});
-
-app.get('/alexa-check/:id', function(req, res) {
-  var id = req.params.id;
-  if (id in cache) {
-    if (cache[id]['downloaded']) {
-      // Video is done downloading
-      res.status(200).send({
-        state: 'success',
-        message: 'Downloaded',
-        downloaded: true
-      });
-    }
-    else {
-      // Video was queried but is still being downloaded
-      res.status(200).send({
-        state: 'success',
-        message: 'Download in progress',
-        downloaded: false
-      });
-    }
-  }
-  else {
-    // No video corresponding to that ID was queried
-    res.status(200).send({
-      state: 'success',
-      message: 'Not in cache'
-    });
-  }
-});
-
-
-//////////////////////////// NON-ALEXA ROUTES ////////////////////////////
-
-function fetch_target_id(req, res) {
-  var id = req.params.id;
-  var url = YOUTUBE_URL_PREFIX + id;
-
-  // Fetch information about the video first
-  ytdl.getInfo(url, function(err, info) {
-    if (err) {
-      res.status(500).json({
-        state: 'error',
-        message: err.message
-      });
-    } else {
-      // Get output file
-      var output_file = path.join(__dirname, 'public', 'site', id + '.mp4');
-      var writer = fs.createWriteStream(output_file);
-
-      // Writer sends response back after finishing
-      writer.on('finish', function() {
-        res.status(200).json({
-          state: 'success',
-          link: '/site/' + id + '.mp4',
-          info: {
-            id: id,
-            title: info.title
-          }
-        });
-      });
-
-      // Pipe out of ytdl stream to the file writer
-      ytdl(url).pipe(writer);
-    }
-  });
-}
-
-app.get('/target/:id', fetch_target_id);
-
-app.get('/search/:query', function(req, res) {
-  // Extract query from request
-  var query = req.params.query;
-
-  // Search YouTube for query
-  ytsearch(query, {
-    maxResults: 1,
-    type: 'video',
-    key: process.env.YOUTUBE_API_KEY
-  }, function(err, results) {
-    if (err) {
-      // An error occurred while searching for the video
-      res.status(500).json({
-        state: 'error',
-        message: err.message
-      });
-    } else {
-      if (!results || !results.length) {
-        // No video found for that query
-        res.status(200).send({
-          state: 'error',
-          message: 'No results found'
-        });
-      } else {
-        // Pass request handling to download function
-        req.params.id = results[0].id;
-        fetch_target_id(req, res);
-      }
-    }
-  });
-});
-
-//////////////////////////////////////////////////////////////////////////
+// Load APIs in separate files 
+require('./api/alexa-v3.js')(app, cache, log);
+require('./api/alexa-v2.js')(app, cache, log);
+require('./api/regular-player.js')(app);
 
 // Start the application!
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  log.info('Node app is running on port', app.get('port'));
 });
